0c25f45077c9e10c18f3a6481c68f14d92476ef2
Wolle1991 <robin.prinz1@gmx.de>
2026-05-14 14:38:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Beispiel:
 // const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx.../exec";
 
-const SCRIPT_URL = "";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx2bX_GGefthXU0S6jimvolYQSEmGn1dBm7yzWqKeb6qPgGFGzO5eHNsA6zkogcDwo/exec";
 
 // Einfacher Admin-Passwortschutz
-const ADMIN_PASSWORD = "triforce";
+const ADMIN_PASSWORD = "triforce1991";
